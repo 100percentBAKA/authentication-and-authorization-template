cc12e6a1c34efc91a17bb55594fa1743b0867fc6
100percentBAKA <1rn21cs011.adarshgs@gmail.com>
2024-10-10 16:35:09 +0530

    impl protected routes

---
+++ client/src/pages/admin.jsx
@@ -0,0 +1,5 @@
+function Admin() {
+  return <div>This page is visible to only Admin</div>;
+}
+
+export default Admin;
---
+++ client/src/pages/pro-user.jsx
@@ -0,0 +1,5 @@
+function ProUser() {
+  return <div>This page is visible only to admin and pro user</div>;
+}
+
+export default ProUser;
